07c4177f2f8132565e8f8d11a1bbc175d0c0c5bf
Pedro Cardoso <pedrodamiao18@gmail.com>
2024-12-11 16:19:13 +0000

    .

--- src/ProdutoFresco.java
+++ src/ProdutoFresco.java
@@ -3,10 +3,10 @@ import java.time.LocalDate;
 //carne, peixe, legumes, fruta
 public class ProdutoFresco extends Produto{
 
-    private String tipo;
+    private String tipoProduto;
 
     public ProdutoFresco(String nome, double preco, int stock, LocalDate validade, double quantidadeunidade, String tipo) {
         super(nome, "Fresco", preco, stock, validade, "Kg", quantidadeunidade);
-        this.tipo = tipo;
+        this.tipoProduto = tipo;
     }
 }
